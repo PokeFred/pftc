60549b47c6b6bc7191262a0c0693ae8ebb80c4ed
PokeFred <cedric.paelmke@gmx.de>
2025-09-07 13:13:57 +0200

    chore: add v0.1.1

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
 
 import pkg from "../package.json" with { type: "json" }
+import { cpSync, renameSync } from "node:fs"
+import { join as joinPath } from "node:path"
 import { Command, program } from "commander"
 import * as prompt from "@clack/prompts"
 import color from "picocolors"
@@ -10,7 +12,7 @@ const create = new Command("create")
     .description("Template Creator")
     .action(async () => {
         console.log()
-        prompt.intro(color.inverse("PokeFred-Templater (Creator)"))
+        prompt.intro("PokeFred-Templater (Creator)")
         prompt.log.warn("feature currently isn't implemented")
         prompt.outro("You're all set!")
     })
@@ -20,8 +22,29 @@ const clean = new Command("clean")
     .description("Template Cleaner")
     .action(async () => {
         console.log()
-        prompt.intro(color.inverse("PokeFred-Templater (Cleaner)"))
+        prompt.intro("PokeFred-Templater (Cleaner)")
         prompt.log.warn("feature currently isn't implemented")
+        prompt.outro("You're all set!")
+    })
+
+const temp = new Command("temp")
+    .description("Template Creator (Temp)")
+    .action(async () => {
+        console.log()
+        prompt.intro("PokeFred-Templater (Creator)")
+
+        let name = null
+        try {
+            name = await prompt.text({
+                message: "Project Name:"
+            })
+        } catch (error) {
+            name = null
+            return
+        }
+        cpSync(joinPath(process.cwd(), "temp"), joinPath(process.cwd(), name), { recursive: true })
+        prompt.log.info("Next Steps:")
+        prompt.log.message(`$ cd ${name}\n$ npm i\n$ npm run dev`)
         prompt.outro("You're all set!")
     })
 
@@ -31,4 +54,5 @@ program
     .helpCommand(true)
     .addCommand(create)
     .addCommand(clean)
+    .addCommand(temp)
     .parse()
