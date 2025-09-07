438282a94fa9131871e2e4f3adb392bdebe48e5c
PokeFred <cedric.paelmke@gmx.de>
2025-09-07 12:02:58 +0200

    chore: add v0.0.1

---
+++ README.md
@@ -0,0 +1,14 @@
+# PokeFred Templater
+
+## How to use
+
+```shell
+$ npx pftc
+  -> print help view
+
+$ npx pftc create
+  -> alias: creator
+
+$ npx pftc clean
+  -> alias: cleaner
+```
--- src/main.js
+++ src/main.js
@@ -1,3 +1,34 @@
 #!/usr/bin/env node
 
-console.log("Hello World")
+import pkg from "../package.json" with { type: "json" }
+import { Command, program } from "commander"
+import * as prompt from "@clack/prompts"
+import color from "picocolors"
+
+const create = new Command("create")
+    .alias("creator")
+    .description("Template Creator")
+    .action(async () => {
+        console.log()
+        prompt.intro(color.inverse("PokeFred-Templater (Creator)"))
+        prompt.log.warn("feature currently isn't implemented")
+        prompt.outro("You're all set!")
+    })
+
+const clean = new Command("clean")
+    .alias("cleaner")
+    .description("Template Cleaner")
+    .action(async () => {
+        console.log()
+        prompt.intro(color.inverse("PokeFred-Templater (Cleaner)"))
+        prompt.log.warn("feature currently isn't implemented")
+        prompt.outro("You're all set!")
+    })
+
+program
+    .name(pkg.name)
+    .version(pkg.version, "-v, --version")
+    .helpCommand(true)
+    .addCommand(create)
+    .addCommand(clean)
+    .parse()
